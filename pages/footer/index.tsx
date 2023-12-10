22f1630d5ac1019356c9bf3f2b7c456c1c2870f7
jaysukh22 <125646448+jaysukh22@users.noreply.github.com>
2023-12-10 22:16:22 +0530

    10-12-2023

--- pages/footer/index.tsx
+++ pages/footer/index.tsx
@@ -1,138 +1,173 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import { FaFacebookF, FaInstagram, FaLinkedinIn, FaRegEnvelopeOpen, FaTwitter } from 'react-icons/fa'
-import { IoLogoDiscord } from 'react-icons/io5'
-import { FiPhone } from 'react-icons/fi'
-import qr from '../../assest/image/footer/qr.png'
-import playstore from '../../assest/image/footer/play-store.png'
-import appstroe from '../../assest/image/footer/app-store.png'
-import cart1 from '../../assest/image/footer/cart-1.png'
-import cart2 from '../../assest/image/footer/cart-2.png'
-import cart3 from '../../assest/image/footer/cart-5.png'
-
-
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaLinkedinIn,
+  FaRegEnvelopeOpen,
+  FaTwitter,
+} from "react-icons/fa";
+import { IoLogoDiscord } from "react-icons/io5";
+import { FiPhone } from "react-icons/fi";
+import { FaWhatsapp } from "react-icons/fa";
+import qr from "../../assest/image/footer/qr.jpg";
+import playstore from "../../assest/image/footer/play-store.png";
+import appstroe from "../../assest/image/footer/app-store.png";
+import cart1 from "../../assest/image/footer/cart-1.png";
+import cart2 from "../../assest/image/footer/cart-2.png";
+import cart3 from "../../assest/image/footer/cart-5.png";
 
 const Footer = () => {
-	return (
-		<footer className='footer_area'>
-			<div className="container">
-				<div className="row">
-					<div className="col-lg-3 col-sm-6">
-						<div className="footer_width">
-							<h5 className='width_title'>Support</h5>
-							<p>685 Market Street,  <br />
-								Las Vegas, LA 95820, <br />
-								United States.</p>
-							<ul>
-								<li> <FaRegEnvelopeOpen className='footer_icon' /> example@domain.com</li>
-								<li><FiPhone className='footer_icon' />(+01) 850-315-5862</li>
+  return (
+    <footer className="footer_area">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-3 col-sm-6">
+            <div className="footer_width">
+              <h5 className="width_title">Support</h5>
 
-							</ul>
-						</div>
-					</div>
-					<div className="col-lg-3 col-sm-6">
-						<div className="footer_width">
-							<h5 className='width_title'>Account</h5>
-							<ul>
-								<li className="navbar_item1">
-									<Link href='' className='navbar_links1'>My Account</Link>
-								</li>
+              <p>
+                South End Circle,
+                <br /> near Ramakrishna Hospital, Jayanagar, <br />
+                Bengaluru, Karnataka 560011
+              </p>
+              <ul>
+                <li>
+                  {" "}
+                  <FaRegEnvelopeOpen className="footer_icon" />{" "}
+                  ssjewelleryandexport@gmail.com
+                </li>
+                <li>
+                  <FiPhone className="footer_icon" />
+                  (+91) 97253-40526
+                </li>
+                <li>
+                  <FaWhatsapp className="footer_icon" />
+                  (+91) 9825464055
+                </li>
+              </ul>
+            </div>
+          </div>
+          {/* <div className="col-lg-3 col-sm-6">
+            <div className="footer_width">
+              <h5 className="width_title">Account</h5>
+              <ul>
+                <li className="navbar_item1">
+                  <Link href="" className="navbar_links1">
+                    My Account
+                  </Link>
+                </li>
 
-								<li className="navbar_item1">
-									<Link href='' className='navbar_links1'>Login / Register</Link>
-								</li>
+                <li className="navbar_item1">
+                  <Link href="" className="navbar_links1">
+                    Login / Register
+                  </Link>
+                </li>
 
-								<li className="navbar_item1">
-									<Link href='' className='navbar_links1'>Cart</Link>
-								</li>
+                <li className="navbar_item1">
+                  <Link href="" className="navbar_links1">
+                    Cart
+                  </Link>
+                </li>
 
-								<li className="navbar_item1">
-									<Link href='' className='navbar_links1'>Wishlist</Link>
-								</li>
-								<li className="navbar_item1">
-									<Link href='' className='navbar_links1'>Shop</Link>
-								</li>
-							</ul>
+                <li className="navbar_item1">
+                  <Link href="" className="navbar_links1">
+                    Wishlist
+                  </Link>
+                </li>
+                <li className="navbar_item1">
+                  <Link href="" className="navbar_links1">
+                    Shop
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div> */}
+          {/* <div className="col-lg-3 col-sm-6">
+            <div className="footer_width">
+              <h5 className="width_title">Quick Link</h5>
+              <ul>
+                <li className="navbar_item1">
+                  <Link href="" className="navbar_links1">
+                    Privacy Policy
+                  </Link>
+                </li>
 
-						</div>
-					</div>
-					<div className="col-lg-3 col-sm-6">
-						<div className="footer_width">
-							<h5 className='width_title'>Quick Link</h5>
-							<ul>
-								<li className="navbar_item1">
-									<Link href='' className='navbar_links1'>Privacy Policy</Link>
-								</li>
+                <li className="navbar_item1">
+                  <Link href="" className="navbar_links1">
+                    Terms Of Use
+                  </Link>
+                </li>
 
-								<li className="navbar_item1">
-									<Link href='' className='navbar_links1'>Terms Of Use</Link>
-								</li>
+                <li className="navbar_item1">
+                  <Link href="" className="navbar_links1">
+                    FAQ
+                  </Link>
+                </li>
 
-								<li className="navbar_item1">
-									<Link href='' className='navbar_links1'>FAQ</Link>
-								</li>
+                <li className="navbar_item1">
+                  <Link href="" className="navbar_links1">
+                    Contact
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div> */}
+          <div className="col-lg-3 col-sm-6">
+            <div className="footer_width">
+              <h5 className="width_title">Chat with whatsapp</h5>
+              <span>scan qr code </span>
+              <div className="btn_group">
+                <div className="qr">
+                  <Image src={qr} alt="" />
+                </div>
+                {/* <div className="app_link">
+                  <Image src={playstore} alt="" className="app" />
+                  <Image src={appstroe} alt="" />
+                </div> */}
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr className="border border-1" />
+        <div className="row align-item-center copy_right">
+          <div className="col-lg-4 col-md-12">
+            <p className="social">
+              <Link href="">
+                <FaFacebookF />
+              </Link>
+              <Link href="">
+                <FaInstagram />
+              </Link>
+              <Link href="">
+                <FaTwitter />
+              </Link>
+              <Link href="">
+                <FaLinkedinIn />
+              </Link>
+              <Link href="">
+                <IoLogoDiscord />
+              </Link>
+            </p>
+          </div>
+          <div className="col-lg-4 col-md-12 d-flex justify-content-center">
+            <p className="quick_link">
+              © {new Date().getFullYear()}. All rights reserved by Axilthemes.
+            </p>
+          </div>
+          <div className="col-lg-4 col-md-12 ">
+            <div className="paymant_img">
+              <span className="quick_link">Accept For</span>
+              <Image src={cart1} alt="" />
+              <Image src={cart2} alt="" />
+              <Image src={cart3} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-								<li className="navbar_item1">
-									<Link href='' className='navbar_links1'>Contact</Link>
-								</li>
-								<li className="navbar_item1">
-									<Link href='' className='navbar_links1'>Contact</Link>
-								</li>
-							</ul>
-
-						</div>
-					</div>
-					<div className="col-lg-3 col-sm-6">
-						<div className="footer_width">
-							<h5 className='width_title'>Download App</h5>
-							<span>Save $3 With App & New User only</span>
-							<div className="btn_group">
-								<div className="qr">
-									<Image src={qr} alt='' />
-								</div>
-								<div className="app_link">
-									<Image src={playstore} alt='' className='app' />
-									<Image src={appstroe} alt='' />
-
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<hr className='border border-1' />
-				<div className="row align-item-center copy_right">
-					<div className="col-lg-4 col-md-12">
-						<p className="social">
-							<Link href=''><FaFacebookF /></Link>
-							<Link href=''><FaInstagram /></Link>
-							<Link href=''><FaTwitter /></Link>
-							<Link href=''><FaLinkedinIn /></Link>
-							<Link href=''><IoLogoDiscord /></Link>
-
-						</p>
-					</div>
-					<div className="col-lg-4 col-md-12 d-flex justify-content-center">
-						<p className='quick_link'>
-							© 2022. All rights reserved by Axilthemes.
-						</p>
-					</div>
-					<div className="col-lg-4 col-md-12 ">
-						<div className="paymant_img">
-							<span className='quick_link'>Accept For</span>
-							<Image src={cart1} alt='' />
-							<Image src={cart2} alt='' />
-							<Image src={cart3} alt='' />
-						</div>
-
-
-
-					</div>
-
-				</div>
-			</div>
-		</footer>
-	)
-}
-
-export default Footer
+export default Footer;
--- pages/slider/index.tsx
+++ pages/slider/index.tsx
@@ -1,59 +1,96 @@
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-import { FaFire } from 'react-icons/fa'
-import { HiOutlineArrowNarrowRight } from 'react-icons/hi'
-import poster1 from '../../assest/image/poster-08.png'
-import poster2 from '../../assest/image/poster-09.png'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { FaFire } from "react-icons/fa";
+import { HiOutlineArrowNarrowRight } from "react-icons/hi";
+import poster1 from "../../assest/image/poster-08.png";
+import poster2 from "../../assest/image/poster-09.png";
+import Header from "../haeder";
+import logo from "../../assest/image/logo.png";
 
 const index = () => {
-	return (
-		<>
-			<main className='slider img_slider align-items-center'>
-				<div className="container">
-					<div className="row align-items-center">
-						<div className="col-sm-8 text">
-							<p className='heading_wrraper'><FaFire className='fire_icon' />Hot Deal In Diamond </p>
-							<h1 className='title'>Exclusive Design Collection</h1>
-							<p className='text-secondary p_wrraper'>Casual line with short design in 100% suede Diamond</p>
-							<button className='shop_btn'>Browse Item <HiOutlineArrowNarrowRight /></button>
-						</div>
-					</div>
-				</div>
-			</main>
+  return (
+    <>
+      <div
+        className="d-flex align-items-center w-100"
+        style={{ display: "flex", justifyContent: "space-between" }}
+      >
+        <Image src={logo} alt="logo" className="hum_burger" />
 
-			<div className="container mt-5">
-				<div className="row">
-					<div className="col-lg-6 ">
-						<div className='singal_poster'>
-							<div className='imgs'>
-								<Image src={poster1} alt='poster' className='rounded-1 img_poster' />
-							</div>
-							<div className='poster'>
-								<h3 className='poster_title'>Premimum  <br />  Quality.</h3>
-								<span className='sub_title'>
-									Collections  <small> <HiOutlineArrowNarrowRight /></small>
-								</span>
-							</div>
-						</div>
-					</div>
-					<div className="col-lg-6 ">
-						<div className='singal_poster'>
-							<div className="imgs">
+        <Header />
+      </div>
 
-								<Image src={poster2} alt='poster' className='rounded-1 img_poster' />
-							</div>
-							<div className='poster1'>
-								<span className='sub_title'>
-									50% Offer In Winter
-								</span>
-								<h3 className='poster_title'>Get Exclusive   <br />   Diamond.</h3>
-							</div>
-						</div>
-					</div>
-				</div></div>
-		</>
-	)
-}
+      <main className="slider img_slider align-items-center">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-sm-8 text">
+              <p className="heading_wrraper">
+                <FaFire className="fire_icon" />
+                Hot Deal In Diamond{" "}
+              </p>
+              <h1 className="title text-white">Exclusive Design Collection</h1>
+              <p className="text-secondary p_wrraper text-white-50">
+                Casual line with short design <br /> in 100% suede Diamond
+              </p>
+           
+                <Link
+                  href="https://api.whatsapp.com/message/4NPDNHWOY5FRP1?autoload=1&app_absent=0"
+                  className="shop_btn text-decoration-none"
+                >
+                  more information <HiOutlineArrowNarrowRight />
+                </Link>
+            
+            </div>
+          </div>
+        </div>
+      </main>
 
-export default index
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-lg-6 ">
+            <div className="singal_poster">
+              <div className="imgs">
+                <Image
+                  src={poster1}
+                  alt="poster"
+                  className="rounded-1 img_poster"
+                />
+              </div>
+              <div className="poster">
+                <h3 className="poster_title">
+                  Premimum <br /> Quality.
+                </h3>
+                <span className="sub_title">
+                  Collections{" "}
+                  <small>
+                    {" "}
+                    <HiOutlineArrowNarrowRight />
+                  </small>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-6 ">
+            <div className="singal_poster">
+              <div className="imgs">
+                <Image
+                  src={poster2}
+                  alt="poster"
+                  className="rounded-1 img_poster"
+                />
+              </div>
+              <div className="poster1">
+                <span className="sub_title">50% Offer In Winter</span>
+                <h3 className="poster_title">
+                  Get Exclusive <br /> Diamond.
+                </h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default index;
